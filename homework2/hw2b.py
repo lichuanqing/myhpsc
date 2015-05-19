c506160d26e83ebf44facd5c07e69e0a3d93b039
LeeTranchin <licq@licq-Satellite-C600.(none)>
2015-05-20 07:51:51 +0800

    modify acoording to demo2.pya: adde check input and test error check

--- homework2/hw2b.py
+++ homework2/hw2b.py
@@ -2,18 +2,30 @@
 
 '''
 make the hw2a.py script more genearl by turning it into a module.
+Demonstration module for quadratic interpolation.
+Update this docstring to describe your code.
+Modified by: Tranchin Lee
 '''
+import numpy as np
+from numpy.linalg import solve
+import matplotlib.pyplot as plt
+
 def quad_interp(xi,yi):
 	'''
-	function to solve polynominal p(x)=c0+c1x+c2x*x,coe vector c will be returned. xi is a array which presentes the x-coordinate of the three points.yi is the y-coordinate.
+	 Quadratic interpolation.  Compute the coefficients of the polynomial
+    interpolating the points (xi[i],yi[i]) for i = 0,1,2.
+    Returns c, an array containing the coefficients of
+      p(x) = c[0] + c[1]*x + c[2]*x**2.
+      
+	(function to solve polynominal p(x)=c0+c1x+c2x*x,coe vector c will be returned. xi is a array which presentes the x-coordinate of the three points.yi is the y-coordinate.)
+	
 	'''
-	import numpy as np
-	from numpy.linalg import solve
-	#import matplotlib.pyplot as plt
-
-	# data points 
-	#xi=np.array([-1.0,1.0,2.0])
-	#yi=np.array([0,4.0,3.0])
+	#chech the input and print error message if not valid input
+	error_message="xi and yi should have type numpy.ndarray"
+	assert (type(xi) is np.ndarray ) and (type(yi) is np.ndarray),error_message
+	
+	error_message="xi and yi should have length 3"
+	assert (len(xi)==3 ) and (len(yi) ==3),error_message
 
 	#Matrix coe. A, Vector coe.b
 	A=np.vstack([np.ones(3),xi,xi**2]).T
@@ -21,20 +33,23 @@ def quad_interp(xi,yi):
 
 	#Solve linear equation for vector c
 	c=solve(A,b)
-	print "the polynominal coefficients are:"
-	print c
+	#print "the polynominal coefficients are:"
+	#print c
 	return c
 	
 def test_quad1():
 	'''
+	Test code, no return value or exception if test runs properly.
 	make a test to the function quad_interp
 	'''
-	from numpy import array
 	
-	xi=array([-1.,0,2])
-	yi=array([1.,-1,7])
+	xi=np.array([-1.,0,2])
+	yi=np.array([1.,-1,7])
 	c=quad_interp(xi,yi)
-	
+	c_true=np.array([-1,0,2.])
+	print "c	 :",c
+	print "c_true:",c_true
+	assert np.allclose(c,c_true),"Incorrect resutls c= %s, Expected c= %s" %(c,c_true)
 if __name__=="__main__":
 	print"Runing test---"
 	test_quad1()
